360bf7cf7b5613394ed5d5314a15d282b4019b2c
big_Xplosion <bigxplosion44@gmail.com>
2014-05-31 20:54:15 +0200

    update submodule + some changes to the build script

--- src/main/java/universalteam/minefall/libs/ModReference.java
+++ src/main/java/universalteam/minefall/libs/ModReference.java
@@ -4,7 +4,7 @@ public class ModReference
 {
 	public static final String MODID = "MineFall";
 	public static final String MODNAME = "MineFall";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "@VERSION@";
 
 	public static final String CHANNEL = MODID;
 
